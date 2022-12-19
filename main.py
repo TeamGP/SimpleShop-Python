8af76c2f2b186ed859b18fb87ef01c7d96f35bbf
ParsaGP <90189271+Parsa-GP@users.noreply.github.com>
2022-12-19 16:25:39 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -1,85 +1,21 @@
-""" TicTacToe """
-from os import system
-import sys
+import base64
 
-table:list = [[None, None, None], [None, None, None], [None, None, None]]
-end_game:bool = False
+def enc_b64(text):
+    t_bytes = text.encode('ascii')
+    b64_bytes = base64.b64encode(t_bytes)
+    return b64_bytes.decode('ascii')
 
-def list_sameitem(lst, eqt):
-    """  """
-    for i in range(len(lst)):
-        if not lst[i] == eqt:
-            return False
-            break
-    else:
-        return True
+prd_list = [
+    {"name":"sib", "price":12_000, "desription":"A delicious fruit", "sell_count":9, "seller":"Mive va tarebar e Avang"},
+    {"name":"medad", "price":9_000, "desription":"Pencil.", "sell_count":7, "seller":"Lavazem tahrir e mamad"},
+    {"name":"gheychi", "price":25_000, "desription":"scissors!", "sell_count":16, "seller":"Bahman chaman zan"},
+    {"name":"noshabe", "price":37_000, "desription":"A delicious drink", "sell_count":4, "seller":"Super market Arshia"}
+]
+users_list = {
+    "parsa1":{"name":"Parsa", "pass":"1234"}
+}
 
-def fancy_table():
-    return f"""
-┌─1─╥─2─╥─3─┐
-1 {table[0][0]} ║ {table[0][1]} ║ {table[0][2]} │
-╞═══╬═══╬═══╡
-2 {table[1][0]} ║ {table[1][1]} ║ {table[1][2]} │
-╞═══╬═══╬═══╡
-3 {table[2][0]} ║ {table[2][1]} ║ {table[2][2]} │
-└───╨───╨───┘
-        """.replace('None', ' ').replace('False', 'X').replace('True', 'O')
-
-def wincheck():
-    win_table = [
-        [table[0][0], table[1][0], table[2][0]],
-        [table[0][1], table[1][1], table[2][1]],
-        [table[0][2], table[1][2], table[2][2]],
-
-        [table[0][0], table[0][1], table[0][2]],
-        [table[1][0], table[1][1], table[1][2]],
-        [table[2][0], table[2][1], table[2][2]],
-
-        [table[0][0], table[1][1], table[2][2]],
-        [table[0][2], table[1][1], table[2][0]]
-    ]
-    global end_game
-    for l in win_table:
-        if list_sameitem(l, True):
-            print('o wins')
-            print('Thanks for playing!')
-            end_game=True
-            break
-        if list_sameitem(l, False):
-            print('x wins')
-            print('Thanks for playing!')
-            end_game=True
-            break
-
-# ┌─1─╥─2─╥─3─┐
-# 1 X ║ O ║ X │
-# ╞═══╬═══╬═══╡
-# 2 O ║ X ║ O │
-# ╞═══╬═══╬═══╡
-# 3 X ║ O ║ X │
-# └───╨───╨───┘
-dafe:int = 0
-
-system('cls')
-print(fancy_table())
-
-while not end_game:
-    wincheck()
-    if end_game: break;
-    if dafe%2==0:
-        try:
-            cord = input('X: ')
-        except KeyboardInterrupt:
-            system('cls')
-            sys.exit()
-        table[int(cord[1])-1][int(cord[0])-1] = False
-    else:
-        try:
-            cord = input('O: ')
-        except KeyboardInterrupt:
-            system('cls')
-            sys.exit()
-        table[int(cord[1])-1][int(cord[0])-1] = True
-    system('cls')
-    print(fancy_table())
-    dafe+=1
+if len(prd_list) < 10:
+    print(prd_list)
+else:
+    print(prd_list[0:10])
