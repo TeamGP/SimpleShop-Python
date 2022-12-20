e809ead1b145b5cb21b2731c835804a55159e92c
ParsaGP <90189271+Parsa-GP@users.noreply.github.com>
2022-12-20 09:28:48 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -6,30 +6,33 @@ def enc_b64(text):
     return b64_bytes.decode('ascii')
 
 prd_list = [
-    {"name":"sib", "price":12_000, "desription":"A delicious fruit", "sell_count":9, "seller":"Mive va tarebar e Avang"},
-    {"name":"medad", "price":9_000, "desription":"Pencil.", "sell_count":7, "seller":"Lavazem tahrir e mamad"},
-    {"name":"gheychi", "price":25_000, "desription":"scissors!", "sell_count":16, "seller":"Bahman chaman zan"},
-    {"name":"noshabe", "price":37_000, "desription":"A delicious drink", "sell_count":4, "seller":"Super market Arshia"}
+    {"name":"sib", "price":12_000, "description":"A delicious fruit", "sell_count":9, "seller":"Mive va tarebar e Avang"},
+    {"name":"medad", "price":9_000, "description":"Pencil.", "sell_count":7, "seller":"Lavazem tahrir e mamad"},
+    {"name":"gheychi", "price":25_000, "description":"scissors!", "sell_count":16, "seller":"Bahman chaman zan"},
+    {"name":"noshabe", "price":37_000, "description":"A delicious drink", "sell_count":4, "seller":"Super market Arshia"}
 ]
 users_list = {
     "parsa1":{"name":"Parsa", "pass":"1234"}
 }
 
 def print_prd(num):
-    for i in len(prd_list):
+    """ prints the products list """
+    if len(prd_list) < 10:
+        prdlst_cut = prd_list
+    else:
+        prdlst_cut = prd_list[num*10-10:num*10]
+
+    for i in prdlst_cut:
         name = i['name']
         price = str(i['price'])
         desc = i['description']
         sell_count = i['sell_count']
         seller = i['seller']
 
-        print(f"""
-        {name} | bought {sell_count} times | {price}$
-        {desc}
-        {seller}
-        """)
-
-if len(prd_list) < 10:
-    print(prd_list)
-else:
-    print(prd_list[0:10])
+        print(f"""==========
+{name} | bought {sell_count} times | {price}$
+{desc}
+{seller}
+""")
+    print('='*10)
+print_prd(1)
