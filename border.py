f0a3c226b688ace9de7b09853128d772efbfc966
ParsaGP <psrzp1386@gmail.com>
2023-01-19 12:23:02 +0330

    More Chars
    Adding two line borders

--- border.py
+++ border.py
@@ -1,4 +1,6 @@
 """ ASCII Borders """
+
+# Single Lines
 lr   = "\u2500" # ─
 ud   = "\u2502" # │
 dr   = "\u250C" # ┌
@@ -10,3 +12,39 @@ udr  = "\u251C" # ┤
 dlr  = "\u252C" # ┬
 ulr  = "\u2534" # ┴
 udlr = "\u253C" # ┼
+
+# Double Lines
+l2r2     = "\u2550" # ═
+u2d2     = "\u2551" # ║
+d2r2     = "\u2554" # ╔
+d2l2     = "\u2557" # ╗
+u2r2     = "\u255A" # ╚
+u2l2     = "\u255D" # ╝
+u2d2l2   = "\u2560" # ╠
+u2d2r2   = "\u2563" # ╣
+d2l2r2   = "\u2566" # ╦
+u2l2r2   = "\u2569" # ╩
+u2d2l2r2 = "\u256C" # ╬
+
+# Combination Of one and two line
+dr2 =    "\u2552" # ╒
+d2r =    "\u2553" # ╓
+dl2 =    "\u2555" # ╕
+d2l =    "\u2556" # ╖
+ur2 =    "\u2558" # ╘
+u2r =    "\u2559" # ╙
+ul2 =    "\u255B" # ╛
+u2l =    "\u255C" # ╜
+udr2 =   "\u255E" # ╞
+u2d2r =  "\u255F" # ╟
+udl2 =   "\u2561" # ╡
+u2d2l =  "\u2562" # ╢
+dl2r2 =  "\u2564" # ╤
+d2lr =   "\u2565" # ╥
+ul2r2 =  "\u2567" # ╧
+u2lr =   "\u2568" # ╨
+udl2r2 = "\u256A" # ╪
+u2d2lr = "\u256B" # ╫
+
+
+
